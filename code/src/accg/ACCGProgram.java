5b28fafff8f9d6a8970aedcc8b0b4a46c2c39856
Thom Castermans <thomcastermans@users.sourceforge.net>
2014-02-11 15:10:35 +0100

    Support Alt+Enter to toggle fullscreen mode, instead of F.
    It would be easy to restore support for F. I also removed an unused import.

--- code/src/accg/ACCGProgram.java
+++ code/src/accg/ACCGProgram.java
@@ -1,12 +1,31 @@
 package accg;
 
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.util.glu.GLU.*;
+import static org.lwjgl.opengl.GL11.GL_AMBIENT_AND_DIFFUSE;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_COLOR_MATERIAL;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_FRONT;
+import static org.lwjgl.opengl.GL11.GL_LIGHT0;
+import static org.lwjgl.opengl.GL11.GL_LIGHTING;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glColorMaterial;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glFlush;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.*;
-import org.lwjgl.util.glu.Sphere;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.opengl.Util;
 
 import accg.camera.Camera;
 import accg.objects.World;
@@ -88,7 +107,14 @@ public class ACCGProgram {
 		while (Keyboard.next()) {
 			if (Keyboard.getEventKeyState()) {
 				switch (Keyboard.getEventKey()) {
-				case Keyboard.KEY_F:
+				case Keyboard.KEY_RETURN:
+					// only respond to Alt+Enter, not just Enter
+					if (Keyboard.getEventKey() == Keyboard.KEY_RETURN &&
+							!(Keyboard.isKeyDown(Keyboard.KEY_LMENU) ||
+							  Keyboard.isKeyDown(Keyboard.KEY_RMENU))) {
+						return;
+					}
+					
 					try {
 						if (Display.isFullscreen()) {
 							Display.setDisplayMode(windowedMode);
@@ -98,7 +124,6 @@ public class ACCGProgram {
 							Display.setFullscreen(true);
 						}
 					} catch (LWJGLException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					break;
