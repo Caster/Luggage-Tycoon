37dc1f49ee296951005bbf9dc962e18ee5aa877a
Willem Sonke <willem3141@users.sourceforge.net>
2014-02-11 14:35:48 +0100

    Add the simulation time to State

--- code/src/accg/State.java
+++ code/src/accg/State.java
@@ -4,6 +4,14 @@ package accg;
  * The state of the program. This for example stores the frame number.
  */
 public class State {
+	
+	/**
+	 * The current frame number.
+	 */
 	public int frame;
 	
+	/**
+	 * The current simulation time in seconds.
+	 */
+	public double time;
 }
