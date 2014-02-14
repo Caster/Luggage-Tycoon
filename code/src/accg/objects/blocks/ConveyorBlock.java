eb88acc44e6b46e1e9ed6f550520dce821c482b5
Willem Sonke <willem3141@users.sourceforge.net>
2014-02-14 15:16:28 +0100

    The conveyor belts move! :D

--- code/src/accg/objects/blocks/ConveyorBlock.java
+++ code/src/accg/objects/blocks/ConveyorBlock.java
@@ -39,14 +39,24 @@ public class ConveyorBlock extends Block {
 		glBegin(GL_QUADS);
 		{
 			glNormal3d(0, 0, 1);
-			glTexCoord2d(0, 0);
+			glTexCoord2d(-s.frame / 50.0, 0);
 			glVertex3d(0.125, 0.125, 0.375);
-			glTexCoord2d(1, 0);
+			glTexCoord2d(2 - s.frame / 50.0, 0);
 			glVertex3d(0.125, 0.875, 0.375);
-			glTexCoord2d(1, 1);
+			glTexCoord2d(2 - s.frame / 50.0, 1);
 			glVertex3d(0.875, 0.875, 0.375);
-			glTexCoord2d(0, 1);
+			glTexCoord2d(-s.frame / 50.0, 1);
 			glVertex3d(0.875, 0.125, 0.375);
+			
+			glNormal3d(0, 0, -1);
+			glTexCoord2d(2 - s.frame / 50.0, 1);
+			glVertex3d(0.125, 0.125, 0.125);
+			glTexCoord2d(-s.frame / 50.0, 1);
+			glVertex3d(0.125, 0.875, 0.125);
+			glTexCoord2d(-s.frame / 50.0, 0);
+			glVertex3d(0.875, 0.875, 0.125);
+			glTexCoord2d(2 - s.frame / 50.0, 0);
+			glVertex3d(0.875, 0.125, 0.125);
 		}
 		glEnd();
 		glDisable(GL_TEXTURE_2D);
