Caster/Luggage-Tycoon

commit b4ea0153b7aa8cd07d2440368a254992b93102b0

Implement draw() in Container already, instead of separate implementations in MenuCollection and MenuBar

code/src/accg/gui/toolkit/Container.java
@@ -1,5 +1,7 @@
 package accg.gui.toolkit;
 
+import static org.lwjgl.opengl.GL11.*;
+
 import java.util.Collection;
 
 import accg.gui.toolkit.event.MouseEvent;
@@ -29,6 +31,26 @@ public abstract class Container extends Component {
 	 * Computes and sets the positions and sizes of the children.
 	 */
 	public abstract void layout();
+	
+	@Override
+	public void draw() {
+		
+		// if needed, compute layout
+		layoutIfNeeded();
+		
+		// actually draw the children
+		for (Component c : getChildren()) {
+			
+			// apply the transformation
+			glPushMatrix();
+			glTranslatef(c.getOutline().getX(), c.getOutline().getY(), 0);
+			
+			c.draw();
+			
+			// restore the transformation
+			glPopMatrix();
+		}
+	}
 	
 	/**
 	 * Returns a list of all children.

code/src/accg/gui/toolkit/MenuCollection.java
@@ -109,25 +109,6 @@ public class MenuCollection extends Container {
 	}
 	
 	@Override
-	public void draw() {
-		
-		// if needed, compute layout
-		layoutIfNeeded();
-		
-		// apply the transformation
-		glPushMatrix();
-		glTranslatef(outline.getX(), outline.getY(), 0);
-		
-		// actually draw the menu (TODO)
-		if (visibleMenu != null) {
-			visibleMenu.draw();
-		}
-		
-		// restore the transformation
-		glPopMatrix();
-	}
-	
-	@Override
 	public int getPreferredWidth() {
 		return visibleMenu.getPreferredWidth();
 	}

code/src/accg/ACCGProgram.java
@@ -281,7 +281,9 @@ public class ACCGProgram {
 			// draw the menu bars
 			GUIUtils.make2D();
 			//gui.outputDebug();
+			glEnable(GL_BLEND);
 			gui.draw();
+			glDisable(GL_BLEND);
 			GUIUtils.make3D();
 			
 			// check for errors

code/src/accg/gui/toolkit/MenuBar.java
@@ -196,22 +196,6 @@ public class MenuBar extends Container {
 	@Override
 	public void draw() {
 		
-		// should we draw in the first place? TODO is this a task for Component?
-		if (!this.visible || items.size() == 0) {
-			return;
-		}
-		
-		// if needed, compute layout
-		layoutIfNeeded();
-		
-		// apply the transformation
-		glPushMatrix();
-		glTranslatef(outline.getX(), outline.getY(), 0);
-		
-		// enable transparency
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		
 		// draw menu bar background, which is semi-transparent
 		glColor4d(1, 1, 1, 0.5);
 		glBegin(GL_QUADS);
@@ -220,16 +204,7 @@ public class MenuBar extends Container {
 		}
 		glEnd();
 		
-		// draw all items
-		for (MenuBarItem item : items) {
-			item.draw();
-		}
-		
-		// disable transparency again
-		glDisable(GL_BLEND);
-		
-		// restore the transformation
-		glPopMatrix();
+		super.draw();
 	}
 
 	@Override

code/src/accg/gui/toolkit/MenuBarItem.java
@@ -140,10 +140,6 @@ public class MenuBarItem extends Component implements Listener {
 			hoverAmount--;
 		}
 		
-		// apply the transformation
-		glPushMatrix();
-		glTranslatef(outline.getX(), outline.getY(), 0);
-		
 		// render hovered background, if needed
 		if ((this.hovered && this.drawHoveredBackground) ||
 				(this.checked && this.drawCheckedBackground)) {
@@ -216,9 +212,6 @@ public class MenuBarItem extends Component implements Listener {
 			glEnd();
 			glDisable(GL_TEXTURE_2D);
 		}
-		
-		// restore the transformation
-		glPopMatrix();
 	}
 	
 	@Override

commit 59ff221e360e21092e40d5633cff7cc37a6fb893

Properly make menu bars invisible if they should be invisible
We forgot to do this, first, resulting in a lot of overlapping menu bars. (This was a regression in b4ea015.)

Also, actually don't draw children when they are invisible. Forgot that as well. :P

code/src/accg/gui/toolkit/Container.java
@@ -41,6 +41,11 @@ public abstract class Container extends Component {
 		// actually draw the children
 		for (Component c : getChildren()) {
 			
+			// do not draw non-visible children
+			if (!c.isVisible()) {
+				continue;
+			}
+			
 			// apply the transformation
 			glPushMatrix();
 			glTranslatef(c.getOutline().getX(), c.getOutline().getY(), 0);

code/src/accg/gui/toolkit/MenuCollection.java
@@ -228,6 +228,11 @@ public class MenuCollection extends Container {
 					+ "contain a menu with key [" + key + "]");
 		}
 		
+		for (MenuBar m : menuBars.values()) {
+			m.setVisible(false);
+		}
+		newMenu.setVisible(true);
+		
 		visibleMenu = newMenu;
 	}
 	