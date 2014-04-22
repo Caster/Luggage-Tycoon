441cdd9680a89a28efe8a25f4b04464ba89041d0
Willem Sonke <willem3141@users.sourceforge.net>
2014-04-22 17:18:53 +0200

    Fix the Ant build script and extend it

---
+++ code/scripts/luggage-tycoon.bat
@@ -0,0 +1,2 @@
+@echo off
+java -Djava.library.path=lib\lwjgl-2.9.1\native\windows\ -jar luggage-tycoon.jar
---
+++ code/scripts/luggage-tycoon.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Djava.library.path=lib/lwjgl-2.9.1/native/linux/ -jar luggage-tycoon.jar
